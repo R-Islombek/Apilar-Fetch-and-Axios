067660dfc8bf26cda4ce67d077c5a353f4053ce7
R-Islombek <islombekraximjanov97dev@gmail.com>
2025-08-13 05:44:27 +0500

    Haqiqiy Api dan Get qildim

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,58 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 const App = () => {
-
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
   async function fetchUsers() {
+    setLoading(true);
+    setError("");
     try {
-      const res = await axios.get("https://jsonplaceholder.typicode.com/users")
-      console.log(res.data);
-      setUsers(res.data)
-    } catch (error) {
-      console.log("Xatolik berayapti", error);
+      const res = await axios.get("https://testpsyedu.limsa.uz/users");
+      console.log("API javobi:", res.data);
+      if (res.data && res.data.data) {
+        setUsers(res.data.data);
+      } else {
+        setUsers([]);
+      }
+    } catch (err) {
+      console.error("Xatolik:", err);
+      setError("Ma'lumotlarni olishda xatolik yuz berdi");
+    } finally {
+      setLoading(false);
     }
   }
 
   useEffect(() => {
-    fetchUsers()
-  }, [])
+    fetchUsers();
+  }, []);
 
   return (
     <div className="container">
-      {users.map((user) => (
-        <div key={user.id} className="card">
-          <img src={`https://picsum.photos/300/300?random=${user.id}`} alt={user.username} className="user-img" />
-          <h1>{user.username}</h1>
-          <p>{user.address.city}</p>
-        </div>
-      ))}
+      {loading ? (
+        <div className="loader">Yuklanmoqda...</div>
+      ) : error ? (
+        <div className="error">{error}</div>
+      ) : (
+        users.map((user) => (
+          <div key={user.id} className="card">
+            <img
+              src={`https://picsum.photos/300/300?random=${user.id}`}
+              alt={`${user.firstName} ${user.lastName}`}
+              className="user-img"
+            />
+            <h1>
+              {user.firstName} {user.middleName} {user.lastName}
+            </h1>
+            <p>{user.cityName}</p>
+          </div>
+        ))
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
